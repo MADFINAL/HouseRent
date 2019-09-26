5a351a6701283b83efdbd4d69c17e0cf3e4c5be7
it18085754 <42891583+it18085754@users.noreply.github.com>
2019-09-26 09:44:19 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/HomePage.java
@@ -0,0 +1,115 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity{
+    TextView Loguser;
+
+
+
+
+
+
+    Button rent,pay,feedback,accountsetting;
+    ImageView imageView2;
+    TextView textView;
+    Animation fromtopbottom;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        fromtopbottom= AnimationUtils.loadAnimation(this,R.anim.fromtopbottom);
+
+        rent=(Button)findViewById(R.id.rent);
+        pay=(Button)findViewById(R.id.pay);
+        feedback=(Button)findViewById(R.id.feedback);
+        textView=(TextView)findViewById(R.id.textView);
+        imageView2=(ImageView) findViewById(R.id.imageView2);
+
+
+        accountsetting =(Button)findViewById(R.id.btnAccountSetting);
+
+
+        imageView2.startAnimation(fromtopbottom);
+        textView.startAnimation(fromtopbottom);
+        rent.startAnimation(fromtopbottom);
+        pay.startAnimation(fromtopbottom);
+        feedback.startAnimation(fromtopbottom);
+
+
+
+        //Chanaka******************************
+
+        Loguser= findViewById(R.id.txtlogedUser);
+
+
+
+
+
+
+
+
+
+
+        //*******************************************************
+
+        accountsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(HomePage.this,userSetting.class);
+                //i.putExtra("email",LoggedUser);
+                startActivity(i);
+
+            }
+        });
+
+        //*********************************************************
+
+
+        //****************************************************
+
+
+
+
+
+
+
+
+        rent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+
+
+                Intent a = new Intent( HomePage.this, MainActivity.class);
+                a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(a);
+            }
+        });
+
+        //Feedback
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+
+
+                Intent a = new Intent( HomePage.this, FeedbackActivity.class);
+                a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(a);
+            }
+        });
+    }
+
+
+
+}
